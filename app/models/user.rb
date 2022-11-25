bacaf92bfb10a187aa80a0cd2501e101aa1bcc70
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-11-25 19:35:11 +0430

    Fixes linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'AuthorId'
 
   validates :Name, presence: true
-  validates :post_counter, numericality: {only_integer: true}
-  validates :post_counter, numericality: {greater_than_or_equal_to: 0}
+  validates :post_counter, numericality: { only_integer: true }
+  validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def return_recent_posts
     posts.order(created_at: :desc).includes(:user).limit(3)
